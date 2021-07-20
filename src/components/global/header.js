246383ffa9fdcbb0f7bd5ead1fd3f791991d9eeb
Pawan <pwnkumar201@gmail.com>
2021-07-21 03:30:16 +0530

    header

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -1,22 +1,33 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 
+// import dark/light switch button
+import SwitchMode from "@components/global/switchMode"
+
 const Header = () => {
   const data = useStaticQuery(graphql`
     query MenuQuery {
       wpMenu(slug: {eq: "primary-menu"}) {
+        id
         menuItems {
           nodes {
             id
-            url
+            parentId
             label
+            url
+            childItems {
+              nodes {
+                url
+                label
+              }
+            }
           }
         }
       }
     }
   `)
 
-  const menuItems = data.wpMenu.menuItems.nodes
+  const menuItems = data.wpMenu.menuItems.nodes ? data.wpMenu.menuItems.nodes.filter(menuItem => menuItem.parentId === null) : null
       
   return (
     <header className="global-header" id="fixedHeader">
@@ -27,7 +38,12 @@ const Header = () => {
             </a>
           </div>
           <div>
-            
+            <div className="primary-menu">
+              {menuItems.map((data) => {
+                return <li key={data.id}><a href={data.url}>{data.label}</a></li>
+              })}
+            </div>
+            <SwitchMode></SwitchMode>  
           </div>          
         </div>        
     </header>
