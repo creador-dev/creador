f85ff7af896ef1c7ebd8383c538f5bf246b2daf4
Pawan Kumar <pwnkumar201@gmail.com>
2021-08-23 13:32:00 +0530

    Mega menu

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import { Link, useStaticQuery, useEffect, graphql } from "gatsby"
 
 // import dark/light switch button
 import SwitchMode from "@components/global/switchMode"
@@ -30,10 +30,41 @@ const Header = () => {
           }
         }
       }
+
+      megaMenu: wpMenu(locations: {eq: GATSBY_MEGA_MENU}) {
+        id
+        menuItems {
+          nodes {
+            id
+            parentId
+            label
+            url
+            childItems {
+              nodes {
+                id
+                url
+                label
+              }
+            }
+          }
+        }
+      }
     }
   `)
 
   const menuItems = data.wpMenu.menuItems.nodes ? data.wpMenu.menuItems.nodes.filter(menuItem => menuItem.parentId === null) : null
+
+  // mega menu items
+  const megaMenuItems = data.megaMenu.menuItems.nodes ? data.megaMenu.menuItems.nodes.filter(menuItem => menuItem.parentId === null) : null
+
+  function hamburgerClick(){
+    const headerElem = document.getElementById("fixedHeader")
+    if(headerElem.classList.contains("show-megamenu")){
+      headerElem.classList.remove("show-megamenu")
+      return
+    }
+    headerElem.classList.add("show-megamenu")
+  }
 
   return (
     <header className="global-header" id="fixedHeader">
@@ -60,6 +91,29 @@ const Header = () => {
             <span className="menu-icon hoverable">
               <SwitchMode></SwitchMode>
             </span>
+            <span className="hamburger-icon">
+              <span className="hamburger" onClick={hamburgerClick}>
+                <span></span>
+              </span>
+            </span>
+            <div className="mega-menu-wrapper">
+              <div className="mega-menu-container">
+                {megaMenuItems.map(item => (
+                  <div key={item.id} className="mega-menu">
+                    <div className="menu-wrap">
+                      <Link className="menu-item hoverable" to={item.url}>{item.label}</Link>
+                      {item.childItems.nodes.length ? 
+                        <ul>
+                          {item.childItems.nodes.map(subItem => (
+                            <li key={subItem.id}><Link className="sub-item hoverable" to={subItem.url}>{subItem.label}</Link></li>
+                          ))}
+                        </ul>
+                      : ''}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
