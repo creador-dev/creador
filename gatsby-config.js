85b6a65aff7c668359659c65e3e3cf1213607345
Pawan Kumar <pwnkumar201@gmail.com>
2021-08-26 12:13:54 +0530

    Added analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -173,7 +173,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "GOOGLE_ANALYTICS_TRACKING_ID",
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Delays sending pageview hits on route update (in milliseconds)
